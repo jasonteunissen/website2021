30d5eca250455e596617ef63c3aa03aa28e8b6d5
jasonteunissen <jasonteunissen@gmail.com>
2020-12-09 08:57:32 +0100

    basic of the stie are there

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,28 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className="main-wrap">
+      <div className="line"></div>
+      <div className="title-wrap">
+        <div className="left">
+          <h1>Jason</h1>
+          <h2>Teunissen</h2>
+        </div>
+        <div className="right">
+          <h3>All-round</h3>
+          <h3>Corporate</h3>
+          <h3>UX Designer</h3>
+        </div>
+      </div>
+      <div className="line"></div>
+
+      <div className="nav nav-right">
+        <a href="htpp://myexternalurl.com">Linkedin</a>
+        <a href="htpp://myexternalurl.com">Dribbble</a>
+        <a href="htpp://myexternalurl.com">Medium</a>
+        <a href="htpp://myexternalurl.com">Podcast</a>
+      </div>
+    </div>
+  );
 }
